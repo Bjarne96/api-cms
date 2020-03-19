b285f1039694a70f2ac08c2a306c2bf89b25d944
Stig <bjatne.abb@gmail.com>
2020-03-19 11:34:56 +0100

    switchted to http server for production

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,13 +4,10 @@ import cors = require("cors");
 import cookieParser = require("cookie-parser");
 import bodyParser = require("body-parser");
 import fs = require("fs");
-import https = require("https");
+import http = require("http");
+
 
 const app = express();
-const privateKey  = fs.readFileSync('certs/server.key', 'utf8'); // find a better way
-const certificate = fs.readFileSync('certs/server.cert', 'utf8'); // find a better way
-const credentials = {key: privateKey, cert: certificate};
-const httpsServer = https.createServer(credentials, app);
 
 // parse body, cookie
 app.use(bodyParser.json())
@@ -20,7 +17,7 @@ app.use(cors({ credentials: true }));
 app.options('*', cors());
 
 //set port
-app.set("port", process.env.PORT || 5000);
+app.set("port", process.env.PORT || 4000);
 
 //something to allow cors or headers?? -> make your selfe know what this is
 app.use(function(req, res, next) {
@@ -33,12 +30,32 @@ app.use(function(req, res, next) {
 require('./routes.js')(app); //loading all routes
 
 //start https server
-const server = httpsServer.listen(app.get("port"), () => {
-    console.log(
-            "App is running in %d in %s mode",
-            app.get("port"),
-            app.get("env")
-    );
-});
+let server;
+if(process.env.MODE === "Development") {
+    const https = require("https")
+    const privateKey  = fs.readFileSync('certs/server.key', 'utf8'); // find a better way
+    const certificate = fs.readFileSync('certs/server.cert', 'utf8'); // find a better way
+    const credentials = {key: privateKey, cert: certificate};
+    const httpsServer = https.createServer(credentials, app);
+    server = httpsServer.listen(app.get("port"), () => {
+        console.log(
+                "App is running in %d in %s mode",
+                app.get("port"),
+                app.get("env")
+        );
+    })
+}else {
+    server = http.createServer(app);
+    server.listen(app.get("port"), () => {
+        console.log(
+                "App is running in %d in %s mode",
+                app.get("port"),
+                app.get("env")
+        );
+    })
+}
+
+
+
 
 export default server;
